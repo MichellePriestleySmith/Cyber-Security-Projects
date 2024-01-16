b17014c734b7a92accb4275493e7c6edab3f9ecc
MichellePriestleySmith <156315267+MichellePriestleySmith@users.noreply.github.com>
2024-01-16 16:04:54 +0000

    Delete Keylogger Project directory

--- Keylogger Project/test.js
+++
@@ -1,9 +0,0 @@
-const keylogger = require("./src/index");
-
-keylogger.start((key, isKeyUp, keyCode) => {
-  console.log("key event", key, isKeyUp, keyCode);
-});
-
-setTimeout(() => {
-  keylogger.stop();
-}, 10000);
